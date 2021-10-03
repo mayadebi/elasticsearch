4fcd185e2d4a8a9546c5fb2d58eaa814afdf06a3
mayadebi <503991583@qq.com>
2021-10-03 21:30:25 +0800

    添加jar包河配置连接

--- src/test/java/cn/itcast/hotel/HotelDemoApplicationTests.java
+++ src/test/java/cn/itcast/hotel/HotelDemoApplicationTests.java
@@ -1,13 +1,33 @@
 package cn.itcast.hotel;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+
 @SpringBootTest
 class HotelDemoApplicationTests {
-
+    private RestHighLevelClient client;
+    //  开始前注入
+    @BeforeEach
+    void setUp(){
+        this.client = new RestHighLevelClient(RestClient.builder(
+                HttpHost.create("http://106.52.4.161:9200")
+        ));
+    }
+    // 结束后关闭
+    @AfterEach
+    void xiaohui() throws IOException {
+        this.client.close();
+    }
     @Test
     void contextLoads() {
+        System.out.println("aaaa");
     }
 
 }
